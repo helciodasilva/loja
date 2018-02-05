54f2c5bc5b242c25de7f6b61ab31515c78941614
Helcio da Silva <helciodasilva@gmail.com>
2018-02-05 00:39:58 -0200

    Configurando o laravel para utilizar a autenticação via JWT

--- config/app.php
+++ config/app.php
@@ -168,6 +168,8 @@ return [
         /*
          * Package Service Providers...
          */
+		 Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+
 
         /*
          * Application Service Providers...
@@ -229,6 +231,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Input' => Illuminate\Support\Facades\Input::class,
+
     ],
 
 ];
